99db57a09e102c900a24d488641cfd0aa2bb1529
pavanchary2222 <136090177+pavanchary2222@users.noreply.github.com>
2023-08-21 00:00:08 +0530

    Add files via upload
    newone

--- follow.c
+++ follow.c
@@ -1,84 +1,75 @@
-#include<stdio.h>
+#include<stdio.h> 
 #include<string.h>
-#include<ctype.h>
-int n,m=0,i=0,j=0;
-char a[10][10],result[10];
+int n,m=0,p,i=0,j=0;
+char a[10][10],followResult[10];
 void follow(char c);
 void first(char c);
-void addtoresult(char);
-void main()
+void addToResult(char);
+int main()
 {
-	int i;
-	int choice;
-	char c;
-	printf("enter no of productions");
-	scanf("%d",&n);
-	for(i=0;i<n;i++){
-		printf("enter p%d :",i+1);
-		scanf("%s",a[i]);
-	}
-	do{
-		m=0;
-		printf("enter character :");
-		scanf(" %c",&c);
-		follow(c);
-		printf("{");
-		for(i=0;i<m;i++)
-			printf("%c ",result[i]);
-			printf("}\n");
-			printf("enter 1 :");
-			scanf("%d",&choice);
-	}
-	while(choice==1);
-	
+ int i;
+ int choice;
+ char c,ch;
+ printf("Enter the no.of productions: ");
+scanf("%d", &n);
+ printf(" Enter %d productions\nProduction with multiple terms should be give as separate productions \n", n);
+ for(i=0;i<n;i++)
+  scanf("%s%c",a[i],&ch);
+    // gets(a[i]);
+ do
+ {
+  m=0;
+  printf("Find FOLLOW of -->");
+  scanf(" %c",&c);
+  follow(c);
+  printf("FOLLOW(%c) = { ",c);
+  for(i=0;i<m;i++)
+   printf("%c ",followResult[i]);
+  printf(" }\n");
+  printf("Do you want to continue(Press 1 to continue....)?");
+ scanf("%d%c",&choice,&ch);
+ }
+ while(choice==1);
 }
 void follow(char c)
 {
-	if(a[0][0]==c)addtoresult('$');
-	for(i=0;i<n;i++)
-	{
-		for(j=2;j<strlen(a[i]);j++)
-		{
-			if(a[i][j]==c){
-				if((a[i][j+1])!='\0')
-					first(a[i][j+1]);
-				if(a[i][j+1]=='\0')
-					follow(a[i][0]);	
-			}
-		}
-	}
+    if(a[0][0]==c)addToResult('$');
+ for(i=0;i<n;i++)
+ {
+  for(j=2;j<strlen(a[i]);j++)
+  {
+   if(a[i][j]==c)
+   {
+    if(a[i][j+1]!='\0')first(a[i][j+1]);
+    if(a[i][j+1]=='\0'&&c!=a[i][0])
+     follow(a[i][0]);
+   }
+  }
+ }
 }
 void first(char c)
 {
-	int k;
-	if(!(isupper(c)))
-		addtoresult(c);
-	for(k=0;k<n;k++)
-	{
-		if(a[k][0]==c)
-		{
-			if(a[k][2]=='$')
-				follow(a[k][0]);
-			else if(islower(a[k][2]))
-				addtoresult(a[k][2]);
-			else
-				first(a[k][2]);
-		}
-	}
+      int k;
+                 if(!(isupper(c)))
+                     //f[m++]=c;
+                     addToResult(c);
+                 for(k=0;k<n;k++)
+                 {
+                 if(a[k][0]==c)
+                 {
+                 if(a[k][2]=='$') follow(a[i][0]);
+                 else if(islower(a[k][2]))
+                     //f[m++]=a[k][2];
+                     addToResult(a[k][2]);
+                 else first(a[k][2]);
+                 }
+                 }
 }
-void addtoresult(char c)
+void  addToResult(char c)
 {
-	int i;
-	for(i=0;i<=m;i++)
-		if(result[i]==c)
-			return;
-  	result[m++]=c;
+    int i;
+    for( i=0;i<=m;i++)
+        if(followResult[i]==c)
+            return;
+   followResult[m++]=c;
 }
-
-/*
-S-aABb
-A-c
-A-$
-B-d
-B-$
-*/
